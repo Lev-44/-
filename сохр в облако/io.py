1be966bb3c37bb568a90db7cd1bfadea8bc1b832
Лев-44 <votonlev@gmail.com>
2024-10-30 18:05:42 +0300

    Сделали возможность загружать из json в окне новом  историю ссылок

--- сохр в облако/io.py
+++ сохр в облако/io.py
@@ -7,6 +7,27 @@ import json
 import requests
 from tkinter import ttk
 history_file="upload_history.json"#создаем имя для файла загрузок
+def show_history():
+    if not os.path.exists (history_file):
+        mb.showinfo("История загрузок","У вас нет истории загрузок")
+        return
+    history_window=Toplevel(window)
+    history_window.title("История загрузок")
+    my_box=Listbox(history_window,width=50,height=20)
+    my_box.grid(row=0,column=0,padx=(10,0),pady=10)
+
+    my_links = Listbox(history_window, width=50, height=20)
+    my_links.grid(row=0, column=1, padx=(0, 10), pady=10)
+
+    with open(history_file, "r") as f:
+        history=json.load(f)#список словарей в json
+        for i in history:
+            my_box.insert(END, i['file_path'])#забираем из json значение по ключю File_path
+            my_links.insert(END, i['file_link'])#забираем из json значение по ключю File_link
+
+
+
+
 def save_history(file_path,link):
      history=[]#пустой список
      if os.path.exists(history_file):#проверяем есть ли хистори файл
@@ -55,6 +76,8 @@ window.geometry("400x500")
 but=ttk.Button(text="Загрузить", command=upload)
 but.pack(pady=10)
 
+his_button=ttk.Button(text="Показать историю", command=show_history)
+his_button.pack()
 
 e=Entry()
 e.pack()
