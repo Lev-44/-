7b0cc8447c6f62fb06cba89bc51f829fd07dfa2e
Лев-44 <votonlev@gmail.com>
2024-10-30 16:47:57 +0300

    Сделали возможность сохранять полученную ссылку в файл- лог upload_history.json в котором сохраняется информация о файле и о ссылке для скачивания в формате списка json

--- сохр в облако/io.py
+++ сохр в облако/io.py
@@ -6,17 +6,17 @@ import os
 import json
 import requests
 from tkinter import ttk
+history_file="upload_history.json"#создаем имя для файла загрузок
+def save_history(file_path,link):
+     history=[]#пустой список
+     if os.path.exists(history_file):#проверяем есть ли хистори файл
+         with open(history_file,"r") as file:#открываем history_file в json
+             history=json.load(file)#загружаем файил и преоьразовываем json в пайтон и закр файл
 
-# def save_history(path,link):
-#     history=[]
-#     if os.path.exists(history_file):
-#         with open(history_file,"r") as file:
-#             history=json.load(file)
+     history.append({"file_path":os.path.basename(file_path), "file_link":link})#добавили новую инфу в history список причем в file_path только имя файла не весь путь тк basename использ
 
-    # history.append({"file_path":os.path.basename(file_path), "file_link":link})
-
-    # with open(history_file,"w") as file:
-    #     json.dump(history,file, indent=6)
+     with open(history_file,"w") as file:# открываем файл для записи
+         json.dump(history,file, indent=4)# сохраняем новую инфу там словарь в формат json
 
 def upload():
     try:
@@ -24,6 +24,7 @@ def upload():
         if filepath:#если не пустая то выполнить ниже
             files={"file":open(filepath,"rb")}#откр файл для чтения по байтно для отправкив сеть 'rb' - Открывает файл в бинарном режиме только для чтения. Указатель файла помещается в начале файла. Это режим "по умолчанию".
             response=requests.post('https://file.io/',files=files)#ответ на запрос post закрытый вид запрса с отправкой данных(строка URL,путь к отправляемому файлу)
+            response.raise_for_status()#для обработки в исключении если будет ошибка при запросе статуса
             if response.status_code==200:#Объект requests.Response модуля requests содержит всю информацию ответа сервера на HTTP-запрос requests.get(), requests.post() и т.д.,если все гуд то поехали далее
                 print(response.headers)# возвратит заголовок сервера
                 s=response.json()#возвратит ответ в виде json
@@ -31,7 +32,10 @@ def upload():
                 if link:#если есть что то в link то поехали
                     e.delete(0,END)#очищаем энтри от говна
                     e.insert(0, link)#вставляем в энтри то что в link
-                    # pyperclip.copy(link)#копир ссылку в буфер обмена
+                    pyperclip.copy(link)  #копир ссылку в буфер обмена link ссылка на скачивание
+                    save_history(filepath,link)#вызываем функцию сохранения в файл JSON и  параметы у нас другие он сам догадывается по местам
+
+                    mb.showinfo("скопировано",f"ссылка{link}скопирована в буфер обмена")
 
                 else:
                     raise ValueError("Нет ссылки")
